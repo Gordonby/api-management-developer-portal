c17c0979c8b053eb6ffdd06cbe2ff7cdd32108d8
Gordon Byers <gordonbyers@gmail.com>
2020-07-31 12:53:28 +0100

    Add files via upload

--- Init-APIM-Dev-Portal.ps1
+++ Init-APIM-Dev-Portal.ps1
@@ -5,14 +5,21 @@
 #2. Do not have a Self Hosted Dev Portal already
 #--------------------------
 #Caveat.
-#This script is PowerShell.  It's not using AZ.  It's not idempotent.  It'll work onetime, and onetime only.
+#This script is PowerShell.  It's not using AZ, and as such it's not idempotent
 
 #User Variables (change these)
 $apimName = "ContosoTravel"
 $apimRg = "ContosoTravel"
 
-#Script variables (don't need to change these)
+#Script variables (you don't need to change these)
+$storageAccountName="" #only change this if you've got a storage account created already, the script will otherwise generate you one
 $storageContainerName="devportal"
+
+#lets clone the repo and jump in
+git clone https://github.com/Azure/api-management-developer-portal.git
+cd api-management-developer-portal
+git checkout 10.11.12
+npm install
 
 #lets check the RG out (we'll need the location for laters)
 $rg = Get-AzResourceGroup $apimRg
@@ -22,42 +29,60 @@ $apimContext = New-AzApiManagementContext -ResourceGroupName $apimRg -ServiceNam
 
 #Lets get the APIM management access key
 $managementAccess = Get-AzApiManagementTenantAccess -Context $apimContext
-if ($managementAccess.Enabled == false) {
+if ($managementAccess.Enabled -eq $false) {
     Set-AzApiManagementTenantAccess -Context $apimContext -Enabled $True
     $managementAccess = Get-AzApiManagementTenantAccess -Context $apimContext
 }
 
 #Now managment Access is enabled, we need a SAS token
-$dateIn30=(Get-Date).AddDays(30).ToShortDateString()
-$expiry=([datetime]::ParseExact($dateIn30,"dd/MM/yyyy",[cultureinfo]::InvariantCulture))
+$sasgenUrl="https://helperfunc.azurewebsites.net/api/CreateApimSAS?code=E2X7dNg1r8eaJnNciqIctaHToHdm5dxq2agPllRaIVWENX4ojM2sDw=="
+$sasgenBody = @{}
+$sasgenBody.Add("id",$managementAccess.Id)
+$sasgenBody.Add("key",$managementAccess.PrimaryKey)
+$sasResponse = Invoke-WebRequest -Method Post -Uri $sasgenUrl -Body $($sasgenBody | ConvertTo-Json)
+$apimSAS = "SharedAccessSignature " + $sasResponse.Content
 
-$dataToSign = $managementAccess.Id + "\n" + $expiry
+#No idea why this didn't work, the signature never came out right.  resorted to the exact same code, in c# in an azure function
+#$dateIn30=(Get-Date).AddDays(30).ToShortDateString()
+#$expiry=([datetime]::ParseExact($dateIn30,"dd/MM/yyyy",[cultureinfo]::InvariantCulture))
+#$expiryString = $expiry.ToString( "yyyy-MM-ddTHH:mm:ss.fffffff" ) #$expiry.ToUniversalTime().ToString( "yyyy-MM-ddTHH:mm:ss.fffffffZ" )
 
-$hmacsha = New-Object System.Security.Cryptography.HMACSHA512
-$hmacsha.key = [Text.Encoding]::ASCII.GetBytes($managementAccess.PrimaryKey)
-$signature = $hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($dataToSign))
-$signature = [Convert]::ToBase64String($signature)
+#$dataToSign = $managementAccess.Id + "\n" + $expiryString
 
-$expiryString = $expiry.ToUniversalTime().ToString( "yyyy-MM-ddTHH:mm:ss.fffffffZ" )
-$apimSAS = "SharedAccessSignature uid=" + $managementAccess.Id + "&ex=" + $expiryString + "&sn=" + $signature
+#$hmacsha = New-Object System.Security.Cryptography.HMACSHA512
+#$hmacsha.key = [Text.Encoding]::UTF8.GetBytes($managementAccess.PrimaryKey)
+#$bytesToHash = [Text.Encoding]::UTF8.GetBytes($dataToSign)
+#$signature = $hmacsha.ComputeHash($bytesToHash)
+#$signature = [Convert]::ToBase64String($signature)
+
+#$apimSAS = "SharedAccessSignature uid=" + $managementAccess.Id + "&ex=" + $expiryString + "&sn=" + $signature
 
 #Test API call, with the APIM SAS token.
-#GET https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ApiManagement/service/{serviceName}/apis?api-version=2019-12-01
-$testURL="https://management.azure.com/subscriptions/$((Get-AzContext).Subscription.id)/resourceGroups/$apimRg/providers/Microsoft.ApiManagement/service/$apimName/apis?api-version=2019-12-01"
+$baseUrl= "https://$ApimName.management.azure-api.net"
+$apiVersion="2014-02-14-preview"
+$testURL = $baseUrl +  "/groups?api-version=" + $apiVersion
 $headers = @{
     Authorization="$apimSAS"
 }
-Invoke-WebRequest -Method Get -Uri $testURL -Headers $headers -UseBasicParsing
+$req=$null
+$req = Invoke-WebRequest -Method Get -Uri $testURL -Headers $headers -UseBasicParsing
+Write-Host "Test API call using the APIM SAS: " + $req.StatusCode + $req.StatusDescription
 
 #Create the storage account
-$rand = Get-Random -Minimum -1000 -Maximum 9999
-$storageAcc = New-AzStorageAccount -ResourceGroupName $apimRg -Name "$apimName$rand".ToLower() -SkuName Standard_LRS -Location $rg.Location
-#$storageAcc = Get-AzStorageAccount -ResourceGroupName $apimRg -Name "ContosoTravel9452"
+if ($storageAccountName -eq "") {
+    $rand = Get-Random -Minimum -1000 -Maximum 9999
+    $storageAcc = New-AzStorageAccount -ResourceGroupName $apimRg -Name "$apimName$rand".ToLower() -SkuName Standard_LRS -Location $rg.Location
+}
+else {
+    $storageAcc = Get-AzStorageAccount -ResourceGroupName $apimRg -Name "$storageAccountName"
+}
+
 $storageContext = $storageAcc.Context
 
 $storageKey = (Get-AzStorageAccountKey -ResourceGroupName $apimrg -Name $storageAcc.StorageAccountName)[0].Value
 $storageConnectionString="DefaultEndpointsProtocol=https;AccountName=" + $storageAcc.StorageAccountName + ";AccountKey=" + $storageKey + ';EndpointSuffix=core.windows.net' 
 
+#Enable Static Website capability
 Enable-AzStorageStaticWebsite -Context $storageContext -IndexDocument "index.html" -ErrorDocument404Path "404/index.html"
 
 #Set the CORS rules on the storage account
@@ -71,7 +96,21 @@ Set-AzStorageCORSRule -Context $storageContext -ServiceType Blob -CorsRules $Cor
 #Create a container for the portal to be published to
 New-AzStorageContainer -Context $storageContext -Name $storageContainerName -Permission Off
 
-$storageSAS = New-AzStorageAccountSASToken -Context $storageContext -Service Blob -ResourceType Container
+#Create a new storage SAS
+$dateIn30=(Get-Date).AddDays(30).ToShortDateString()
+$expiry=([datetime]::ParseExact($dateIn30,"dd/MM/yyyy",[cultureinfo]::InvariantCulture))
+$storageSAS = New-AzStorageAccountSASToken -Context $storageContext -Service Blob -ResourceType Container,Object -ExpiryTime $expiry -Permission "racwdlup"
+
+#Test storageSAS actually can upload something (just a ranom image is being used)
+$tempfile=[System.IO.Path]::GetTempPath() + "test-image.png"
+Invoke-WebRequest -uri "https://gordon.byers.me/assets/img/die-bart-die.png" -OutFile $tempfile
+$testfile = Get-Content $tempfile -Raw
+$storageAcc.PrimaryEndpoints.Blob + $storageSAS
+$uri = $storageAcc.PrimaryEndpoints.Blob + $storageContainerName + "/test-image.png" + $storageSAS
+$headers = @{}
+$headers.Add("x-ms-blob-type","BlockBlob")
+$storageTestResult = Invoke-WebRequest -uri $uri -Method Put -Body $file -ContentType "image/png"  -Headers $headers
+Write-Host "Test image upload using Storage SAS: " + $storageTestResult.StatusCode + " " + $storageTestResult.StatusDescription
 
 #Portal Config file output
 $configdesignjson = (Get-Content ("./src/config.design.json") | ConvertFrom-Json)
@@ -80,7 +119,7 @@ $configdesignjson.managementApiAccessToken = $apimSAS
 $configdesignjson.blobStorageContainer=$storageContainerName
 $configdesignjson.blobStorageUrl=$storageAcc.PrimaryEndpoints.Blob + $storageSAS 
 $configdesignjson.backendUrl = $configdesignjson.backendUrl.Replace("<service-name>",$apimName)
-$configdesignjson | ConvertTo-Json | Out-File "./src/config.design.json"
+$configdesignjson | ConvertTo-Json | % { [System.Text.RegularExpressions.Regex]::Unescape($_) } | Out-File "./src/config.design.json" -Encoding "UTF8"
 
 #Config Publish Json
 $configpublishjson = (Get-Content ("./src/config.publish.json") | ConvertFrom-Json)
@@ -88,14 +127,14 @@ $configpublishjson.managementApiUrl = $configpublishjson.managementApiUrl.Replac
 $configpublishjson.managementApiAccessToken = $apimSAS 
 $configpublishjson.blobStorageContainer=$storageContainerName
 $configpublishjson.blobStorageConnectionString=$storageConnectionString
-$configpublishjson | ConvertTo-Json | Out-File "./src/config.publish.json"
+$configpublishjson | ConvertTo-Json | % { [System.Text.RegularExpressions.Regex]::Unescape($_) } | Out-File "./src/config.publish.json" -Encoding "UTF8"
 
 #Config runtime json
 $configruntimejson = (Get-Content ("./src/config.runtime.json") | ConvertFrom-Json)
 $configruntimejson.managementApiUrl = $configruntimejson.managementApiUrl.Replace("<service-name>",$apimName)
 $configruntimejson.backendUrl = $configruntimejson.backendUrl.Replace("<service-name>",$apimName)
 $configruntimejson.proxyHostnames = $configruntimejson.proxyHostnames.Replace("<service-name>",$apimName)
-$configruntimejson | ConvertTo-Json | Out-File "./src/config.runtime.json"
+$configruntimejson | ConvertTo-Json | % { [System.Text.RegularExpressions.Regex]::Unescape($_) } | Out-File "./src/config.runtime.json"  -Encoding "UTF8"
 
 #generate.bat
 $generatebat = Get-Content ("./scripts/generate.bat")
@@ -103,9 +142,11 @@ For ($i=0; $i -lt $generatebat.Length; $i++) {
     if ($generatebat[$i].StartsWith("set management_endpoint")) { $generatebat[$i]='set management_endpoint="' + $apimname + '.management.azure-api.net"' }
     if ($generatebat[$i].StartsWith("set access_token")) { $generatebat[$i]='set access_token="' + $configpublishjson.managementApiAccessToken + '"' }
     if ($generatebat[$i].StartsWith("set storage_connection_string")) { $generatebat[$i]='set storage_connection_string="' + $storageConnectionString + '"' }
+    
 }
-$generatebat | Out-File "./scripts/generate.bat"
+$generatebat | Out-File "./scripts/generate.bat" -Encoding "UTF8"
 
+#start the generation
 Start-Process "./scripts/generate.bat"
 
 #Run the portal
